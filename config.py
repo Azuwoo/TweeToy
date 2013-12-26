41b030bad8a01c9978b1aef6b04f4fcf5dc138b3
Azuwoo <azumazua@gmail.com>
2013-12-26 10:52:32 +0900

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-twDict = {
-    'consumer_key' : 'your consumer_key',
-    'consumer_secret' : 'consumer_secret',
-    'access_token_key' : 'access_token_key',
-    'access_token_secret' : 'access_token_secret'
-}
